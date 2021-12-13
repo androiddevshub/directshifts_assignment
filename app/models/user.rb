d65e561da33c90344e0d473f53f65a24f74c2543
Shubham jain <beardo@Shubhams-MacBook-Pro-2.local>
2021-12-13 16:04:12 +0530

    Add user and user_token model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  def user_data(token)
+    {
+      id: id,
+      name: name,
+      email: email,
+      token: token,
+    }
+  end       
+end
